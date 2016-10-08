48e48dc4363edb76b057a422eddd289aa5bbd77e
Kenny Leung <kenny_leung@pobox.com>
2016-10-08 15:31:26 -0700

    initialize jsonkvc

---
+++ jsonkvc/jsonkvc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  jsonkvc
+//
+//  Created by Kenny Leung on 2016-10-08.
+//  Copyright © 2016 PepperDog Enterprises. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
